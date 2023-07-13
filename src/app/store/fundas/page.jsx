5b06e9fbeb7c0ce17bcd72ea60f9636d6ef14838
iairkap <iairkap@gmail.com>
2023-07-13 19:40:13 -0300

    tree

--- src/app/store/fundas/page.jsx
+++ src/app/store/fundas/page.jsx
@@ -7,11 +7,15 @@ import CardsContainer from "../../components/cardsContainer/cardsContainer";
 import NavBar from "../../components/navBar/navBar";
 import Cart from "@/app/components/cart/cart";
 import styles from "./general.module.css";
+import { useSession } from "next-auth/react";
 
 function Fundas() {
+  const [session, loading] = useSession();
   const { covers, isLoading, fullColor, cubrevalijas, maletines } = useContext(
     GlobalContext
   );
+
+  console.log("Datos de la sesión:", session);
 
   return (
     <div>
