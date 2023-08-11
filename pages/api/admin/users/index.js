1f690769b81e7c12ad3f1d3541f577e928cd5890
Rai5559 <icexbee1@gmail.com>
2023-08-11 17:18:21 -0600

    merged

--- pages/api/admin/users/index.js
+++ pages/api/admin/users/index.js
@@ -4,24 +4,70 @@ import verifyMiddleware from "../../jwt-session/verifyMiddleware";
 async function handler(req, res, verifyMethod) {
   if (verifyMethod === process.env.ADMIN_EMAIL) {
     if (req.method === "GET") {
-      const users = await prisma.user.findMany({
-        include: {
-          orders: true,
-        },
-      });
+      const { search, orderBy } = req.query;
+      let users;
+      let orderArguments = {};
+
+      if (orderBy) {
+        switch (orderBy) {
+          case "name":
+            orderArguments = {
+              name: "asc",
+            };
+            break;
+          case "location":
+            orderArguments = {
+              city: "asc",
+            };
+            break;
+          case "orderDate":
+            orderArguments = {
+              orders: {
+                date: "desc",
+              },
+            };
+            break;
+          default:
+            break;
+        }
+      }
+
+      if (search) {
+        users = await prisma.user.findMany({
+          where: {
+            OR: [
+              { name: { contains: search, mode: "insensitive" } },
+              { lastName: { contains: search, mode: "insensitive" } },
+              { city: { contains: search, mode: "insensitive" } },
+              // Agrega otros campos si los necesitas
+            ],
+          },
+          include: {
+            orders: true,
+          },
+        });
+      } else {
+        users = await prisma.user.findMany({
+          include: {
+            orders: true,
+          },
+          orderBy: orderArguments,
+        });
+      }
+
       users.forEach((user) => (user.password = undefined));
       users.forEach((user) => (user.ordersNumber = user.orders.length));
       res.status(200).json(users);
     } else if (req.method === "DELETE") {
-      const email = req.query.email;
+      const userId = req.query.id;
 
-      if (!email) {
+      if (!userId) {
         res.status(400).json({ message: "User ID required" });
       }
 
       try {
         const user = await prisma.user.delete({
-          where: { email: email },
+          where: { id: Number(userId) },
         });
 
         user.password = undefined;
@@ -30,8 +76,6 @@ async function handler(req, res, verifyMethod) {
         console.error(error);
         res.status(500).json({ message: "Error deleting user" });
       }
-    } else {
-      res.status(405).json({ message: "Method not allowed" });
     }
   } else {
     res.status(403).json({ message: "Not authorized" });
