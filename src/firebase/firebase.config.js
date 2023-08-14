2ff902cc8911fd990a348448143794a1e8bc967b
iairkap <iairkap@gmail.com>
2023-08-14 18:26:37 -0300

    windows

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCbJBIAFDUphzN2s8KxvVFM1eQTva4U-LE",
   authDomain: "real-cover.firebaseapp.com",
@@ -18,8 +13,13 @@ const firebaseConfig = {
   measurementId: "G-RQFL8DH01N",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+let analytics;
+
+if (typeof window !== "undefined" && !firebase.apps.length) {
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  analytics = getAnalytics(app);
+}
 
 export const auth = getAuth(app);
