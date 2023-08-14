7b7d5467630bb27518c9c9b37f460b20eb2b2705
iairkap <iairkap@gmail.com>
2023-08-14 15:14:33 -0300

    deploy

--- src/app/components/PedidosCard/pedidoscardcontainer.jsx
+++ src/app/components/PedidosCard/pedidoscardcontainer.jsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import PedidosCard from "./PedidosCard";
+import PedidosCard from "./pedidoscard";
 import styles from "./pedidoscontainer.module.css";
 
 function PedidosContainer({ user }) {
