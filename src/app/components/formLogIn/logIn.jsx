69ac8f63680f473e2ca0282902a5a90e278322e1
iairkap <iairkap@gmail.com>
2023-08-17 14:50:48 -0300

    logIn

--- src/app/components/formLogIn/logIn.jsx
+++ src/app/components/formLogIn/logIn.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import styles from "./logIn.module.css";
-/* import ForgotPasswordModal from "../ForgetPasswordModal/ForgetPasswordModal";
- */
-import { useRouter, usePathname } from "next/navigation"; // Importa esto
-import { useContext } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation"; // Cambiado de "next/navigation" a "next/router"
 import { GlobalContext } from "./../../store/layout";
+
 import namecontact from "../../../../public/name-contact-form.svg";
 import mailcontact from "../../../../public/mail-contact-form.svg";
 import namecontactverde from "../../../../public/name-contact-form-verde.svg";
 import mailcontactverde from "../../../../public/mail-contact-form-verde.svg";
-import Image from "next/image";
+
 function LogIn({ toggleForm }) {
   const { setIsAuthenticated } = useContext(GlobalContext);
 
@@ -18,45 +17,6 @@ function LogIn({ toggleForm }) {
   const router = useRouter();
   const [nombreFocused, setNombreFocused] = useState(false);
   const [emailFocused, setEmailFocused] = useState(false);
-
-  /*  const handleGoogleLogin = async () => {
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
-
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      const idToken = await user.getIdToken();
-
-      try {
-        const res = await fetch("/api/login-google", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ idToken }),
-        });
-
-        if (res.status === 200 && data.user) {
-          localStorage.setItem("user", JSON.stringify(data.user));
-          console.log("data.user", data.user);
-          router.push("/store/fundas");
-          setIsAuthenticated(true);
-        } else {
-          // El inicio de sesión falló. Mostrar un mensaje de error al usuario.
-          const resData = await res.json();
-          alert(resData.message);
-        }
-      } catch (error) {
-        console.error("Error al iniciar sesión con Google:", error);
-        // Mostrar un mensaje de error al usuario.
-        alert(`Error al iniciar sesión con Google: ${error.message}`);
-      }
-    } catch (error) {
-      console.error("Error al iniciar sesión con Google:", error);
-      alert(`Error al iniciar sesión con Google: ${error.message}`);
-    }
-  }; */
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -69,33 +29,30 @@ function LogIn({ toggleForm }) {
     });
     const data = await res.json();
     if (res.status === 200) {
-      // Aquí se almacena la información en el localStorage
       localStorage.setItem("user", JSON.stringify(data.user));
-      console.log("data.user", data.user);
-
       router.push("/store/fundas");
       setIsAuthenticated(true);
     } else {
-      // Aquí deberías manejar el error en caso de que la respuesta no sea 200
       console.error(
         "Error en el inicio de sesión:",
         data.message || "Error desconocido"
-      ); // Mostramos el error en la consola
+      );
     }
   };
 
-  const openModal = () => setIsModalOpen(true);
+  const openModal = () => setIsModalOpen(true); // Esta función parece que necesita más contexto.
 
   return (
     <div className={styles.generalContainer}>
-      <h1 className={styles.tlte}>Bienvenido de vuelta a Real Cover!</h1>
+      <h1 className={styles.tlte}>Bienvenido!</h1>
       <div className={styles.inputContenedor}>
-        <h2>Inicia sesion para proseguir con la compra</h2>
+        <h2 className={styles.subtitles}>
+          Inicia sesion para proseguir con la compra
+        </h2>
         <span className={styles.formSubs}>Email</span>
         <div className={styles.inputContainer}>
           <input
             type="email"
-            placeholder=""
             value={email}
             className={styles.input}
             onChange={(e) => setEmail(e.target.value)}
@@ -114,7 +71,6 @@ function LogIn({ toggleForm }) {
         <div className={styles.inputContainer}>
           <input
             type="password"
-            placeholder=""
             value={password}
             className={styles.input}
             onChange={(e) => setPassword(e.target.value)}
@@ -136,11 +92,12 @@ function LogIn({ toggleForm }) {
           Crear Cuenta Nueva
         </button>
         <div className={styles.logInContainerThird}>
-          {/*             <Image src={Google} alt="image" onClick={handleGoogleLogin} />
-           */}{" "}
+          {/* Aquí podrías agregar el login de Google nuevamente si es necesario */}
         </div>
         <div>
-          <span onClick={openModal}>Olvidaste la contraseña?</span>
+          <span className={styles.olvidaste} onClick={openModal}>
+            Olvidaste la contraseña?
+          </span>
         </div>
       </div>
     </div>
