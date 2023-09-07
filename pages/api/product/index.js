c8bd6a0df7a981f1e66c741c688a013ab7d800b9
Rai5559 <icexbee1@gmail.com>
2023-09-06 18:09:39 -0600

    Paginates and type filter for products to optimize

--- pages/api/product/index.js
+++ pages/api/product/index.js
@@ -1,12 +1,21 @@
 import prisma from "../../../prisma/client";
-import mapAndSaveImages from "../firebase/mapnsave";
 
 export default async function handler(req, res) {
   const { method } = req;
+  const { productType } = req.query;
+  const { page } = req.query;
+  const { limit } = req.query;
 
   switch (method) {
     case "GET":
-      const products = await prisma.product.findMany();
+      const products = await prisma.product.findMany({
+        where: {
+          productType: productType,
+        },
+        skip: (page - 1) * limit,
+        take: limit,
+      });
+
       return res.status(200).json(products);
 
     default:
